09f560ae1a516b84c2b5dbdb462df04db9881659
Sascha Schimke <sascha@schimke.me>
2018-02-26 21:26:37 +0100

    a

---
+++ src/EventDispatcherInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of foobar.
+ *
+ * (c) Sascha Schimke <sascha@schimke.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Sci\Bar;
+
+interface EventDispatcherInterface
+{
+    public function dispatchEvent($event);
+}
---
+++ src/Foo/Domain/ReadModel/Foo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of foobar.
+ *
+ * (c) Sascha Schimke <sascha@schimke.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Sci\Bar\Foo\Domain\readModel;
+
+
+class Foo
+{
+
+}
